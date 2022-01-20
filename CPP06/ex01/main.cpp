3f86e1f8ae3aad73f82ee2ceae9d52cbf8b3ee3d
Julien Andre <jandre@fu-r1-p8.s19.be>
2022-01-20 15:52:38 +0100

    ok

---
+++ CPP06/ex01/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jandre <jandre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 15:33:30 by jandre            #+#    #+#             */
+/*   Updated: 2022/01/20 15:51:39 by jandre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdint>
+#include <iostream>
+
+typedef struct Data {
+    int i;
+    char c;
+    const char *str;
+}               Data;
+
+uintptr_t serialize(Data *ptr)
+{
+    return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data *deserialize(uintptr_t raw)
+{
+    return (reinterpret_cast<Data *>(raw));
+}
+
+int main()
+{
+    Data *data = new Data;
+    Data *data2;
+    uintptr_t i;
+
+    data->c = 'd';
+    data->str = "wqe";
+    data->i = 8;
+    std::cout << "before serialisation : " << data->c << ", " << data->str << ", " << data->i << std::endl << std::endl;
+    i = serialize(data);
+    std::cout << "uintptr_t : " << i << std::endl << std::endl;
+    data2 = deserialize(i);
+    std::cout << "after deserialisation : " << data2->c << ", " << data2->str << ", " << data2->i << std::endl;
+    delete data;
+    return (0);
+}
