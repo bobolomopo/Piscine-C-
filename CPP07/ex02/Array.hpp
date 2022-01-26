efde0cb265ca2d35dd4b6d7e8d8751fcf9100be0
Julien Andre <jandre@fu-r1-p8.s19.be>
2022-01-26 18:57:36 +0100

    ok

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,15 +6,17 @@
 /*   By: jandre <jandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 18:02:31 by jandre            #+#    #+#             */
-/*   Updated: 2022/01/26 16:52:54 by jandre           ###   ########.fr       */
+/*   Updated: 2022/01/26 18:56:59 by jandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 # include <iostream>
+# include <stdexcept>
+# include <exception>
 
-class outOfRange : std::exception
+class outOfRange : public std::exception
 {
     public:
         virtual const char* what() const throw()
@@ -33,7 +35,7 @@ class Array
             std::cout << "Array created with size 0" << std::endl;
             return ;
         }
-        Array<T>(unsigned int n) : _n(n), _array(new T[_n]())
+        Array<T>(unsigned int n) : _n(n), _array(new T[n]())
         {
             std::cout << "Array created with size : " << this->_n << std::endl;
             return ;
@@ -53,21 +55,21 @@ class Array
         }
 
         //operators overloads
-        T &operator[](unsigned int n) const
+        T & operator[](unsigned int n) const
         {
-            if (n > this->size())
+            if (n >= this->size())
                 throw (outOfRange());
             return (this->_array[n]);
         }
-        Array<T> &operator=(Array<T> const &rhs)
+        
+        Array<T> & operator=(Array<T> const &rhs)
         {
             if (this != &rhs)
             {
                 this->_n = rhs._n;
-                T *array = new T[rhs.size()]();
+                this->_array = new T[rhs.size()]();
                 for (unsigned int i = 0; i < rhs._n; i++)
-                    array[i] = rhs[i];
-                this->_array = array;
+                    this->_array[i] = rhs[i];
             }
             return (*this);
         }
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,55 +1,69 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jandre <jandre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 18:50:52 by jandre            #+#    #+#             */
+/*   Updated: 2022/01/26 18:51:57 by jandre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Array.hpp"
 
-#define MAX_VAL 10
-int main(int, char**)
+int main(void)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[16];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cout << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-   /* try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }*/
+	Array<int> a = Array<int>(3);
+	for (unsigned int i = 0; i < a.size(); i++) {
+		std::cout << a[i] << std::endl;
+	}
 
-    for (int i = 0; i < MAX_VAL - 1; i++)
-    {
-        numbers[i] = rand();
-        std::cout << numbers[i] << ", ";
-    }
-    std::cout << numbers[MAX_VAL] << std::endl;
-    delete [] mirror;//
-    return 0;
+	std::cout << "-----------" << std::endl;
+
+	Array<char> b = Array<char>(3);
+	b[0] = 'a';
+	b[1] = 'b';
+	b[2] = 'c';
+
+	for (unsigned int i = 0; i < b.size(); i++)
+		std::cout << b[i] << std::endl;
+
+	std::cout << "-----------" << std::endl;
+
+	Array<char> c = Array<char>(b);
+
+	for (unsigned int i = 0; i < c.size(); i++)
+		std::cout << c[i] << std::endl;
+
+	std::cout << "-----------" << std::endl;
+
+	Array<std::string> str1 = Array<std::string>();
+	Array<std::string> str2 = Array<std::string>(4);
+	str2[0] = "how";
+	str2[1] = "are";
+	str2[2] = "you";
+	str2[3] = "?";
+
+	str1 = str2;
+	for (int i = 0; i < 4; i++)
+		std::cout << str1[i] << " ";
+	std::cout << std::endl << "Size = " << str1.size() << std::endl;
+
+	std::cout << "-----------" << std::endl;
+
+	try{
+		a[80];
+		b[80];
+		c[80];
+	}
+	catch (std::exception & exception){
+		std::cout << exception.what() << std::endl;
+		return 0;
+	}
+
+	std::cout << "-----------" << std::endl;
+
+	return 0;
 }
