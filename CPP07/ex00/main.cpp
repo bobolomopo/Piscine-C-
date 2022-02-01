55b7b0a469a72e61f1ef08ea08cfb436ec20fa0b
Julien Andre <jandre@fu-r7-p5.s19.be>
2022-02-01 16:00:59 +0100

    ok

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jandre <jandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 17:28:30 by jandre            #+#    #+#             */
-/*   Updated: 2022/01/21 18:02:59 by jandre           ###   ########.fr       */
+/*   Updated: 2022/02/01 15:59:52 by jandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,21 @@ int main()
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    b = a;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    double a1 = 29889.32;
+    double b1 = 3.123;
+    ::swap( a, b );
+    std::cout << "a = " << a1 << ", b = " << b1 << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a1, b1 ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a1, b1 ) << std::endl;
+    float a2 = 2123123;
+    float b2 = 3.22222;
+    ::swap( a, b );
+    std::cout << "a = " << a2 << ", b = " << b2 << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a2, b2 ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a2, b2 ) << std::endl;
     std::string c = "chaine1";
     std::string d = "chaine2";
     ::swap(c, d);
